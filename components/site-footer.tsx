09cc3496f9cf5d9ae80d19103fd5516aea4116c6
Shashi Bhushan Jha <brightdigigold@Shashis-MacBook-Air.local>
2025-11-06 16:44:35 +0530

    calculation done also done

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -10,7 +10,7 @@ export function SiteFooter() {
 
   return (
     <footer className="text-white bg-[#117ABA]">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10 md:py-14">
+      <div className=" px-4 sm:px-6 lg:px-12 py-10 md:py-14">
 
         {/* Top grid */}
         <div className="grid gap-10 md:grid-cols-3 lg:grid-cols-4 items-start">
