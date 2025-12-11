cdc1c10f640dbbff80b2b65be5aa7e945591e3aa
Shashi Bhushan Jha <brightdigigold@Shashis-MacBook-Air.local>
2025-12-11 11:26:13 +0530

    investors page SHAREHOLDERS POLICY DONE

--- app/flexitube-business/page.tsx
+++ app/flexitube-business/page.tsx
@@ -2,7 +2,8 @@
 import { SiteHeader } from "@/components/site-header"
 import { SiteFooter } from "@/components/site-footer"
 import { BusinessPage } from "@/components/business-page"
-import FlexiComp from "@/components/Flexible Tubes"
+import FlexiComp from "@/components/flexitubes"
+
 
 export default function Page() {
   return (
